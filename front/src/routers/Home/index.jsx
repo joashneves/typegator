3235f023d9aac7e7656b79ade415dde13b16f701
joashneves <joashneves@gmail.com>
2025-01-17 11:21:48 -0300

    implementa front com back end

--- front/src/routers/Home/index.jsx
+++ front/src/routers/Home/index.jsx
@@ -1,23 +1,46 @@
+import { useEffect, useState } from "react";
 import Header from "../../components/header";
 import LinkPost from "../../components/LinkPost";
 import Pesquisa from "../../components/pesquisa";
+import axios from "axios";
 
 export default function Home() {
+  const [links, setLinks] = useState([]); 
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchLinks = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/link");
+        setLinks(response.data); 
+        setLoading(false);
+      } catch (error) {
+        console.error("Erro ao buscar os links:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchLinks();
+  }, []); 
+
   return (
     <>
       <Header />
-      <br></br>
+      <br />
       <Pesquisa />
-      <br></br>
-      <LinkPost
-        titulo={"Titulo Teste"}
-        descricao={
-          "descriçaõ klwfkonwioedfn iqwudn iuqwme ioqfmoi ioqmd oif iomio qmoim qwoidm qwiod "
-        }
-        link={
-          "https://www.devmedia.com.br/react-js-criando-rotas-com-react-router-dom/42901"
-        }
-      />
+      <br />
+      {loading ? (
+        <p>Carregando links...</p>
+      ) : (
+        links.map((link, index) => (
+          <LinkPost
+            key={index}
+            titulo={link.titulo}
+            descricao={link.descricao}
+            link={link.link}
+          />
+        ))
+      )}
     </>
   );
 }
