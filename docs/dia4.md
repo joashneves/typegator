2d25b50be552728b9ae27ce79f1cbd5eaf172d6d
joashneves <joashneves@gmail.com>
2025-01-18 18:08:07 -0300

    coloca o build

--- docs/dia4.md
+++ docs/dia4.md
@@ -2,3 +2,4 @@
 
 - Configurando package.json
 - Configurando a API e o front para rodarem em uma unica url
+- Configurado a square com o github
--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 require('dotenv').config();
 const app = require('./src/app.js');
 const manipulador404 = require('./src/middlewares/manipulador404.js');
-const manipuladorDeErros = require('./src/middlewares/manipuladorDeErros.js')
+const manipuladorDeErros = require('./src/middlewares/manipuladorDeErros.js');
 const express = require('express');
 const { auth } = require('express-openid-connect');
-const path = require('path')
-//JWT
+const path = require('path');
+const { exec } = require('child_process');
+const fs = require('fs');
+
+// Verifica se a pasta 'front/dist' existe antes de rodar o build
+const buildDir = path.join(__dirname, 'front', 'dist');
+
+if (!fs.existsSync(buildDir)) {
+  console.log('Pasta build não encontrada. Iniciando o build...');
+  exec('cd front && npm run build', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Erro ao realizar o build: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`Erro no build: ${stderr}`);
+      return;
+    }
+    console.log(`Build realizado com sucesso: ${stdout}`);
+  });
+} else {
+  console.log('Pasta build já existe. Servindo os arquivos estáticos.');
+}
+
+// Configuração do JWT
 const config = {
   authRequired: false,
   auth0Logout: true,
@@ -14,6 +37,7 @@ const config = {
   clientID: process.env.CLIENT_ID,
   issuerBaseURL: process.env.ISSUER_BASE_URL
 };
+
 app.use(auth(config));
 
 function setCorsHeaders(req, res, next) {
@@ -22,12 +46,13 @@ function setCorsHeaders(req, res, next) {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Cabeçalhos permitidos
   next();
 }
+
 // Servindo arquivos estáticos da pasta 'front/dist'
-app.use(express.static(path.join(__dirname, 'front', 'dist')));
+app.use(express.static(buildDir));
 
 // Rota para a raiz (/) que serve o index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'front', 'dist', 'index.html'));
+  res.sendFile(path.join(buildDir, 'index.html'));
 });
 
 app.options('*', (req, res) => {
@@ -44,7 +69,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(manipuladorDeErros);
 app.use(manipulador404);
-app.listen(PORT, () => {
-  console.log('servidor escutando!');
-});
 
+// Iniciando o servidor e mostrando a URL
+app.listen(PORT, () => {
+  console.log(`Servidor escutando em http://localhost:${PORT}`);
+});
