ca4da0eabbc41bd9333d98ac970957a61bb0983e
joashneves <joashneves@gmail.com>
2025-01-18 15:50:55 -0300

    coloca script de build

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Carrega variáveis de ambiente
+const API_URL = process.env.VITE_API_URL || 'http://localhost:3000';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // URL do backend
+        target: API_URL, // URL do backend
         changeOrigin: true, // Altera a origem para evitar problemas de CORS
         rewrite: (path) => path.replace(/^\/api/, ''), // Remove o prefixo /api
       },
